e855c25d0460040fcab607ad287b9c682a3a8c78
rshig <90143161+rshigg@users.noreply.github.com>
2022-08-14 21:16:50 -0230

    upgrade deps and add prettier config

---
+++ app/routes/lobby/$lobbyId.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import type { LoaderFunction } from '@remix-run/cloudflare';
+
+export const loader: LoaderFunction = () => {
+	return {};
+};
+
+export default function Lobby() {
+	return null;
+}
